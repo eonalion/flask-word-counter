6e3bb6e417b59603704d498d1b3a57e18c6ee152
Polina Suboch <palina.subach@gmail.com>
2022-03-31 19:01:15 +0400

    Add model and migrations into the app

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
-app.config.from_object(env_config)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+from models import Result
 
 @app.route("/")
 def index():
     return "Hello User! Welcome to Flask counter example app."
+
+
+@app.route("/<name>")
+def hello_name(name):
+    return f"Hello {name}"
+
+
+if __name__ == '__main__':
+    app.run()
--- config.py
+++ config.py
@@ -1,16 +1,28 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     DEBUG = False
-    DEVELOPMENT = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://", 1)
+
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
+
 
 class StagingConfig(Config):
+    DEVELOPMENT = True
     DEBUG = True
 
+
 class DevelopmentConfig(Config):
-    DEBUG = True
     DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
